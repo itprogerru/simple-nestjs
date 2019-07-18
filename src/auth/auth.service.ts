35bf07ebe61311fef99c991fe1b75286c9a6a646
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2019-07-18 10:10:39 +0400

    singIn авторизаия

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -9,7 +9,15 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async singIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    if (!username) {
+     throw new UnauthorizedException('Invalid credentials');
+   }
   }
 }
